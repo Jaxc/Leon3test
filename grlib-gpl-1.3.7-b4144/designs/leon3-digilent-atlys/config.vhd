db443eef134956d3996c1294af15ef86cc5d2b03
Jaxc <cooljaxc@gmail.com>
2014-06-07 16:15:26 +0200

    updated leon

--- grlib-gpl-1.3.7-b4144/designs/leon3-digilent-atlys/config.vhd
+++ grlib-gpl-1.3.7-b4144/designs/leon3-digilent-atlys/config.vhd
@@ -23,9 +23,9 @@ package config is
   constant CFG_CLK_NOFB : integer := 0;
 -- LEON3 processor core
   constant CFG_LEON3 : integer := 1;
-  constant CFG_NCPU : integer := (2);
+  constant CFG_NCPU : integer := (1);
   constant CFG_NWIN : integer := (8);
-  constant CFG_V8 : integer := 16#32# + 4*0;
+  constant CFG_V8 : integer := 2 + 4*0;
   constant CFG_MAC : integer := 0;
   constant CFG_BP : integer := 1;
   constant CFG_SVT : integer := 1;
@@ -37,8 +37,8 @@ package config is
   constant CFG_FPU : integer := 0 + 16*0 + 32*0;
   constant CFG_GRFPUSH : integer := 0;
   constant CFG_ICEN : integer := 1;
-  constant CFG_ISETS : integer := 2;
-  constant CFG_ISETSZ : integer := 8;
+  constant CFG_ISETS : integer := 1;
+  constant CFG_ISETSZ : integer := 4;
   constant CFG_ILINE : integer := 8;
   constant CFG_IREPL : integer := 0;
   constant CFG_ILOCK : integer := 0;
@@ -46,25 +46,25 @@ package config is
   constant CFG_ILRAMADDR: integer := 16#8E#;
   constant CFG_ILRAMSZ : integer := 1;
   constant CFG_DCEN : integer := 1;
-  constant CFG_DSETS : integer := 2;
+  constant CFG_DSETS : integer := 1;
   constant CFG_DSETSZ : integer := 4;
-  constant CFG_DLINE : integer := 4;
+  constant CFG_DLINE : integer := 8;
   constant CFG_DREPL : integer := 0;
   constant CFG_DLOCK : integer := 0;
-  constant CFG_DSNOOP : integer := 1*2 + 4*0;
+  constant CFG_DSNOOP : integer := 1*2 + 4*1;
   constant CFG_DFIXED : integer := 16#0#;
   constant CFG_DLRAMEN : integer := 0;
   constant CFG_DLRAMADDR: integer := 16#8F#;
   constant CFG_DLRAMSZ : integer := 1;
   constant CFG_MMUEN : integer := 1;
   constant CFG_ITLBNUM : integer := 8;
-  constant CFG_DTLBNUM : integer := 8;
-  constant CFG_TLB_TYPE : integer := 0 + 1*2;
-  constant CFG_TLB_REP : integer := 0;
+  constant CFG_DTLBNUM : integer := 2;
+  constant CFG_TLB_TYPE : integer := 1 + 0*2;
+  constant CFG_TLB_REP : integer := 1;
   constant CFG_MMU_PAGE : integer := 0;
   constant CFG_DSU : integer := 1;
-  constant CFG_ITBSZ : integer := 4;
-  constant CFG_ATBSZ : integer := 4;
+  constant CFG_ITBSZ : integer := 1;
+  constant CFG_ATBSZ : integer := 0;
   constant CFG_LEON3FT_EN : integer := 0;
   constant CFG_IUFT_EN : integer := 0;
   constant CFG_FPUFT_EN : integer := 0;
@@ -76,7 +76,7 @@ package config is
   constant CFG_PCLOW : integer := 2;
 -- AMBA settings
   constant CFG_DEFMST : integer := (0);
-  constant CFG_RROBIN : integer := 1;
+  constant CFG_RROBIN : integer := 0;
   constant CFG_SPLIT : integer := 0;
   constant CFG_FPNPEN : integer := 0;
   constant CFG_AHBIO : integer := 16#FFF#;
@@ -88,14 +88,14 @@ package config is
 -- DSU UART
   constant CFG_AHB_UART : integer := 0;
 -- JTAG based DSU interface
-  constant CFG_AHB_JTAG : integer := 1;
+  constant CFG_AHB_JTAG : integer := 0;
 -- Ethernet DSU
   constant CFG_DSU_ETH : integer := 1 + 0 + 0;
   constant CFG_ETH_BUF : integer := 2;
   constant CFG_ETH_IPM : integer := 16#C0A8#;
   constant CFG_ETH_IPL : integer := 16#0033#;
-  constant CFG_ETH_ENM : integer := 16#020765#;
-  constant CFG_ETH_ENL : integer := 16#003456#;
+  constant CFG_ETH_ENM : integer := 16#020000#;
+  constant CFG_ETH_ENL : integer := 16#000000#;
 -- LEON2 memory controller
   constant CFG_MCTRL_LEON2 : integer := 0;
   constant CFG_MCTRL_RAM8BIT : integer := 0;
@@ -107,27 +107,27 @@ package config is
   constant CFG_MCTRL_SD64 : integer := 0;
   constant CFG_MCTRL_PAGE : integer := 0 + 0;
 -- DDR controller
-  constant CFG_DDR2SP : integer := 1;
-  constant CFG_DDR2SP_INIT : integer := 1;
+  constant CFG_DDR2SP : integer := 0;
+  constant CFG_DDR2SP_INIT : integer := 0;
   constant CFG_DDR2SP_FREQ : integer := 100;
-  constant CFG_DDR2SP_TRFC : integer := (130);
-  constant CFG_DDR2SP_DATAWIDTH : integer := (16);
+  constant CFG_DDR2SP_TRFC : integer := 130;
+  constant CFG_DDR2SP_DATAWIDTH : integer := 64;
   constant CFG_DDR2SP_FTEN : integer := 0;
   constant CFG_DDR2SP_FTWIDTH : integer := 0;
-  constant CFG_DDR2SP_COL : integer := (10);
-  constant CFG_DDR2SP_SIZE : integer := (128);
-  constant CFG_DDR2SP_DELAY0 : integer := (0);
-  constant CFG_DDR2SP_DELAY1 : integer := (0);
-  constant CFG_DDR2SP_DELAY2 : integer := (0);
-  constant CFG_DDR2SP_DELAY3 : integer := (0);
-  constant CFG_DDR2SP_DELAY4 : integer := (0);
-  constant CFG_DDR2SP_DELAY5 : integer := (0);
-  constant CFG_DDR2SP_DELAY6 : integer := (0);
-  constant CFG_DDR2SP_DELAY7 : integer := (0);
-  constant CFG_DDR2SP_NOSYNC : integer := 1;
+  constant CFG_DDR2SP_COL : integer := 9;
+  constant CFG_DDR2SP_SIZE : integer := 8;
+  constant CFG_DDR2SP_DELAY0 : integer := 0;
+  constant CFG_DDR2SP_DELAY1 : integer := 0;
+  constant CFG_DDR2SP_DELAY2 : integer := 0;
+  constant CFG_DDR2SP_DELAY3 : integer := 0;
+  constant CFG_DDR2SP_DELAY4 : integer := 0;
+  constant CFG_DDR2SP_DELAY5 : integer := 0;
+  constant CFG_DDR2SP_DELAY6 : integer := 0;
+  constant CFG_DDR2SP_DELAY7 : integer := 0;
+  constant CFG_DDR2SP_NOSYNC : integer := 0;
 -- AHB status register
-  constant CFG_AHBSTAT : integer := 1;
-  constant CFG_AHBSTATN : integer := (1);
+  constant CFG_AHBSTAT : integer := 0;
+  constant CFG_AHBSTATN : integer := 1;
 -- AHB ROM
   constant CFG_AHBROMEN : integer := 1;
   constant CFG_AHBROPIP : integer := 0;
@@ -136,45 +136,45 @@ package config is
   constant CFG_ROMMASK : integer := 16#E00# + 16#100#;
 -- AHB RAM
   constant CFG_AHBRAMEN : integer := 1;
-  constant CFG_AHBRSZ : integer := 16;
+  constant CFG_AHBRSZ : integer := 4;
   constant CFG_AHBRADDR : integer := 16#A00#;
   constant CFG_AHBRPIPE : integer := 0;
 -- Gaisler Ethernet core
   constant CFG_GRETH : integer := 1;
   constant CFG_GRETH1G : integer := 0;
-  constant CFG_ETH_FIFO : integer := 32;
+  constant CFG_ETH_FIFO : integer := 8;
 -- UART 1
-  constant CFG_UART1_ENABLE : integer := 1;
-  constant CFG_UART1_FIFO : integer := 8;
+  constant CFG_UART1_ENABLE : integer := 0;
+  constant CFG_UART1_FIFO : integer := 1;
 -- LEON3 interrupt controller
   constant CFG_IRQ3_ENABLE : integer := 1;
   constant CFG_IRQ3_NSEC : integer := 0;
 -- Modular timer
-  constant CFG_GPT_ENABLE : integer := 1;
-  constant CFG_GPT_NTIM : integer := (2);
-  constant CFG_GPT_SW : integer := (8);
-  constant CFG_GPT_TW : integer := (32);
-  constant CFG_GPT_IRQ : integer := (8);
-  constant CFG_GPT_SEPIRQ : integer := 1;
+  constant CFG_GPT_ENABLE : integer := 0;
+  constant CFG_GPT_NTIM : integer := 1;
+  constant CFG_GPT_SW : integer := 8;
+  constant CFG_GPT_TW : integer := 8;
+  constant CFG_GPT_IRQ : integer := 8;
+  constant CFG_GPT_SEPIRQ : integer := 0;
   constant CFG_GPT_WDOGEN : integer := 0;
   constant CFG_GPT_WDOG : integer := 16#0#;
 -- GPIO port
   constant CFG_GRGPIO_ENABLE : integer := 1;
   constant CFG_GRGPIO_IMASK : integer := 16#0000#;
-  constant CFG_GRGPIO_WIDTH : integer := (32);
+  constant CFG_GRGPIO_WIDTH : integer := (2);
 -- VGA and PS2/ interface
-  constant CFG_KBD_ENABLE : integer := 1;
-  constant CFG_VGA_ENABLE : integer := 1;
+  constant CFG_KBD_ENABLE : integer := 0;
+  constant CFG_VGA_ENABLE : integer := 0;
   constant CFG_SVGA_ENABLE : integer := 0;
 -- SPI memory controller
-  constant CFG_SPIMCTRL : integer := 1;
+  constant CFG_SPIMCTRL : integer := 0;
   constant CFG_SPIMCTRL_SDCARD : integer := 0;
-  constant CFG_SPIMCTRL_READCMD : integer := 16#03#;
+  constant CFG_SPIMCTRL_READCMD : integer := 16#0#;
   constant CFG_SPIMCTRL_DUMMYBYTE : integer := 0;
   constant CFG_SPIMCTRL_DUALOUTPUT : integer := 0;
-  constant CFG_SPIMCTRL_SCALER : integer := (1);
-  constant CFG_SPIMCTRL_ASCALER : integer := (8);
-  constant CFG_SPIMCTRL_PWRUPCNT : integer := (30000);
+  constant CFG_SPIMCTRL_SCALER : integer := 1;
+  constant CFG_SPIMCTRL_ASCALER : integer := 1;
+  constant CFG_SPIMCTRL_PWRUPCNT : integer := 0;
   constant CFG_SPIMCTRL_OFFSET : integer := 16#0#;
 -- GRLIB debugging
   constant CFG_DUART : integer := 0;
